58d2fda4cf472e87d0446095ef4b05aece0f7a49
blainefreestone <blainefreestone01@gmail.com>
2024-06-29 14:32:38 -0600

    updated prompt and included new parameter for grammar concept

--- app/graphs/generate_question.py
+++ app/graphs/generate_question.py
@@ -5,13 +5,20 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers.string import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
+system_prompt = "You are a Latin teacher and you need to generate a " \
+"question (in Latin) based on the passage given by the student." \
+"The question should only contain Latin words, unless the non-Latin word is found in the passage itself."
+"The question should be able to be answered utilizing the {target_grammar} and the content of the passage." \
+"The question should not ask for any information that is not in the passage."
+
 class State(TypedDict):
     passage: str
+    target_grammar: str
     generated_question: str
 
 llm = ChatOpenAI()
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a Latin teacher and you need to generate a question (in Latin) based on the passage given by the student."),
+    ("system", system_prompt),
     ("user", "Passage:\n\n{passage}")
 ])
 chain = prompt | llm | StrOutputParser()
