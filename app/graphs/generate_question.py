4f54d46143cc94bf55b21de9b44950b42660aac5
blainefreestone <blainefreestone01@gmail.com>
2024-06-29 14:50:06 -0600

    updated structure to generate insight from passage using grammar concept, and then generate a question based on the insight

--- app/graphs/generate_question.py
+++ app/graphs/generate_question.py
@@ -5,32 +5,53 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers.string import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
-system_prompt = "You are a Latin teacher and you need to generate a " \
-"question (in Latin) based on the passage given by the student." \
-"The question should only contain Latin words, unless the non-Latin word is found in the passage itself."
-"The question should be able to be answered utilizing the {target_grammar} and the content of the passage." \
-"The question should not ask for any information that is not in the passage."
+system_question_prompt = "You are a Latin expert and you need to generate a question (in Latin) " \
+"based on a passage and an insight about the passage given by the user. " \
+
+system_insight_prompt = "You are a Latin expert and you need to generate an insight based " \
+"on the passage given by the user. " \
+"The insight should utilize the grammar concept given by the user. " \
+"The insight should not contain any information that is not in the passage."
 
 class State(TypedDict):
     passage: str
     target_grammar: str
+    generated_insight: str
     generated_question: str
 
 llm = ChatOpenAI()
-prompt = ChatPromptTemplate.from_messages([
-    ("system", system_prompt),
-    ("user", "Passage:\n\n{passage}")
+
+question_prompt = ChatPromptTemplate.from_messages([
+    ("system", system_question_prompt),
+    ("user", "Passage:\n\n{passage}\n\nInsight:\n\n{generated_insight}")
 ])
-chain = prompt | llm | StrOutputParser()
+question_chain = question_prompt | llm | StrOutputParser()
+
+insight_prompt = ChatPromptTemplate.from_messages([
+    ("system", system_insight_prompt),
+    ("user", "Passage:\n\n{passage}\n\nTarget grammar concept: {target_grammar}")
+])
+insight_chain = insight_prompt | llm | StrOutputParser()
 
 def get_graph():
     graph_builder = StateGraph(State)
     graph_builder.add_node("generate_question", generate_question)
-    graph_builder.set_entry_point("generate_question")
+    graph_builder.add_node("generate_insight", generate_insight)
+
+    graph_builder.set_entry_point("generate_insight")
+
+    graph_builder.add_edge("generate_insight", "generate_question")
 
     return graph_builder.compile()
 
 def generate_question(state: State) -> State:
-    return {'generated_question': chain.invoke({
-        'passage': state['passage']
+    return {'generated_question': question_chain.invoke({
+        'passage': state['passage'],
+        'generated_insight': state['generated_insight']
+    })}
+
+def generate_insight(state: State) -> State:
+    return {'generated_insight': insight_chain.invoke({
+        'passage': state['passage'],
+        'target_grammar': state['target_grammar']
     })}
