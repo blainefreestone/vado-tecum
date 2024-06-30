27535c11c5ab81500d7abaee8083924119ea010e
blainefreestone <blainefreestone01@gmail.com>
2024-06-29 18:44:30 -0600

    function for returning prompts added

--- app/graphs/graphs.py
+++ app/graphs/graphs.py
@@ -4,3 +4,6 @@ from . import generate_question
 
 def get_generate_question_graph():
     return generate_question.get_graph()
+
+def get_generate_question_prompts():
+    return generate_question.get_prompts()
--- tests/graphs/test_graphs.py
+++ tests/graphs/test_graphs.py
@@ -5,3 +5,7 @@ from langgraph.graph.state import CompiledGraph
 def test_get_generate_question_graph():
     result = graphs.get_generate_question_graph()
     assert isinstance(result, CompiledGraph)
+
+def test_get_generate_question_prompts():
+    result = graphs.get_generate_question_prompts()
+    assert isinstance(result, dict)
