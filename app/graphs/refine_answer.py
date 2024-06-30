f0a0e4b752d4d45d8fac5b780eebffaa1750d5ec
blainefreestone <blainefreestone01@gmail.com>
2024-06-29 21:24:04 -0600

    added grading capability and did prompt engineering

--- app/graphs/refine_answer.py
+++ app/graphs/refine_answer.py
@@ -1,17 +1,61 @@
+from typing import Annotated
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph
+from langchain_core.prompts import ChatPromptTemplate
+from langgraph.graph.message import add_messages
 from langchain_openai import ChatOpenAI
+from typing import Optional
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+system_grade_prompt = "You are a Latin expert and you need to grade a student's answer to a question. " \
+"The question is based on a passage and intends to test the student's understanding of the following passage and ability to utilize the target grammar concept. " \
+"Question: {generated_question}\n\n" \
+"Passage: {passage}\n\n" \
+"Target grammar concept: {target_grammar}\n\n" \
+
+class AnswerGrade(BaseModel):
+    correctness: float = Field(..., ge=0, le=1, description="If the information in the answer is supported by the passage (ignoring the question).")
+    relevance: float = Field(..., ge=0, le=1, description="If the response is relevant to the question asked.")
+    use_of_grammar_target: float = Field(..., ge=0, le=1, description="If the response correctly uses the target grammar concept.")
 
 class State(TypedDict):
     passage: str
     target_grammar: str
-    # generated_insight: str (maybe need, gonna try it without it first)
     generated_question: str
+    answer: str
+    # messages: Annotated[list, add_messages]
+    grade: AnswerGrade
 
 llm = ChatOpenAI(model="gpt-4o")
+structured_llm = llm.with_structured_output(AnswerGrade)
+
+grade_prompt = ChatPromptTemplate.from_messages([
+    ("system", system_grade_prompt),
+    ("user", "Answer:\n\n{answer}")
+])
+
+grade_chain = grade_prompt | structured_llm
 
 def get_prompts():
-    return {}
+    return {
+        'grade': grade_prompt
+    }
 
 def get_graph():
-    pass
+    graph_builder = StateGraph(State)
+
+    graph_builder.add_node("grade_answer", grade_answer)
+
+    graph_builder.set_entry_point("grade_answer")
+
+    return graph_builder.compile()
+
+def grade_answer(state: State) -> State:
+    return {
+        'grade': grade_chain.invoke({
+            'generated_question': state['generated_question'],
+            'passage': state['passage'],
+            'answer': state['answer'],
+            'target_grammar': state['target_grammar']
+        })
+    }
