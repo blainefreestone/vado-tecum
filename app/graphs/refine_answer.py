5de8134d2181b540a2855ef48e2d24532e05a9d7
blainefreestone <blainefreestone01@gmail.com>
2024-06-29 21:58:59 -0600

    created laud_answer node

--- app/graphs/refine_answer.py
+++ app/graphs/refine_answer.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph
+from langchain_core.output_parsers.string import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langgraph.graph.message import add_messages
 from langchain_openai import ChatOpenAI
@@ -12,7 +13,14 @@ system_grade_prompt = "You are a Latin expert and you need to grade a student's 
 "The question is based on a passage and intends to test the student's understanding of the following passage and ability to utilize the target grammar concept. " \
 "Question: {generated_question}\n\n" \
 "Passage: {passage}\n\n" \
+"Target grammar concept: {target_grammar}\n\n"
+
+system_laud_answer_prompt = "You are a helpful Latin teacher and you need to provide feedback to a student. " \
+"Question: {generated_question}\n\n" \
+"Passage: {passage}\n\n" \
 "Target grammar concept: {target_grammar}\n\n" \
+"The student will give you a good answer. "
+"Laud the student's answer and ability, and provide constructive feedback (all in Latin). " \
 
 class AnswerGrade(BaseModel):
     correctness: float = Field(..., ge=0, le=1, description="How correct is the information in the answer based on the passage?")
@@ -39,6 +47,13 @@ grade_prompt = ChatPromptTemplate.from_messages([
 
 grade_chain = grade_prompt | structured_llm
 
+laud_answer_prompt = ChatPromptTemplate.from_messages([
+    ("system", system_laud_answer_prompt),
+    ("user", "Answer:\n\n{answer}")
+])
+
+laud_answer_chain = laud_answer_prompt | llm | StrOutputParser()
+
 def get_prompts():
     return {
         'grade': grade_prompt
@@ -48,8 +63,14 @@ def get_graph():
     graph_builder = StateGraph(State)
 
     graph_builder.add_node("grade_answer", grade_answer)
+    graph_builder.add_node("laud_answer", laud_answer)
 
     graph_builder.set_entry_point("grade_answer")
+
+    graph_builder.add_conditional_edges(
+        "grade_answer",
+        refine_router
+    )
 
     return graph_builder.compile()
 
@@ -64,7 +85,8 @@ def grade_answer(state: State) -> State:
     }
 
 def refine_router(state: State) -> Literal[
-    "refine_correctness", "refine_relevance", 
+    "refine_correctness", 
+    "refine_relevance", 
     "refine_use_of_grammar_target", 
     "refine_correct_use_of_grammar_target", 
     "refine_fluency",
@@ -96,3 +118,11 @@ def refine_router(state: State) -> Literal[
         return "refine_correct_use_of_grammar_target"
     elif lowest_score == grade.fluency:
         return "refine_fluency"
+    
+def laud_answer(state: State) -> State:
+    return {"comment": laud_answer_chain.invoke({
+        "answer": state["answer"],
+        "generated_question": state["generated_question"],
+        "passage": state["passage"],
+        "target_grammar": state["target_grammar"]
+    })}
