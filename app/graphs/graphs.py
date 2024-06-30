19aeadd31ed76c093940dcad9d838772fd0fb883
blainefreestone <blainefreestone01@gmail.com>
2024-06-29 21:24:14 -0600

    added refine_answer functions

--- app/graphs/graphs.py
+++ app/graphs/graphs.py
@@ -1,9 +1,16 @@
 # graphs.py
 
 from . import generate_question
+from . import refine_answer
 
 def get_generate_question_graph():
     return generate_question.get_graph()
 
 def get_generate_question_prompts():
     return generate_question.get_prompts()
+
+def get_refine_answer_graph():
+    return refine_answer.get_graph()
+
+def get_refine_answer_prompts():
+    return refine_answer.get_prompts()
