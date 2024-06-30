d0499eca72d1ea834e23b4f699fd470b42a2ae32
blainefreestone <blainefreestone01@gmail.com>
2024-06-29 19:02:58 -0600

    added token count and total cost to evaluation metrics

--- evaluations/base_graph_evaluation.py
+++ evaluations/base_graph_evaluation.py
@@ -1,5 +1,6 @@
 import time
 import pprint
+from langchain_community.callbacks import get_openai_callback
 
 class BaseGraphEvaluation:
     name = "BaseGraphEvaluation"
@@ -20,9 +21,14 @@ class BaseGraphEvaluation:
     def run_evaluation(self, test_func, *args, **kwargs):
         print(f"Running {test_func.__name__}...")
         start_time = time.time()  # Capture start time
-        result = test_func(*args, **kwargs)
+        with get_openai_callback() as cb:
+            result = test_func(*args, **kwargs)
+            total_tokens = cb.total_tokens
+            total_cost = cb.total_cost
         end_time = time.time()  # Capture end time
         duration = end_time - start_time  # Calculate duration
         print("Result: ", end="")
         pprint.pprint(result, indent=4)
-        print(f"Test completed in {duration:.2f} seconds.\n")
+        print(f"Test completed in {duration:.2f} seconds.")
+        print(f"Total tokens: {total_tokens}")
+        print(f"Total cost: {total_cost}\n")
