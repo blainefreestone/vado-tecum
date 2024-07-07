37444e7f1f7549b5b93c1e1d4abeee37442a800e
blainefreestone <blainefreestone01@gmail.com>
2024-07-07 14:58:42 -0600

    corrected loading and saving paths

--- app/__main__.py
+++ app/__main__.py
@@ -4,12 +4,12 @@ import yaml
 config = yaml.safe_load(open("config.yaml"))
 
 if __name__ == "__main__":
-    if App.load("app.pickle"):
-        app = App.load(config["app_save_path"])
+    if App.load(config["app_save_path"] + "/app.pickle"):
+        app = App.load(config["app_save_path"] + "/app.pickle")
     else:
         app = App()
         app.add_text_files(config['texts_path'])
 
     app.run()
 
-    app.save(config["app_save_path"])
+    app.save(config["app_save_path"] + "/app.pickle")
