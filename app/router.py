21dd10a204ff655310ff7a3a2a5cfb151725da04
blainefreestone <blainefreestone01@gmail.com>
2024-08-13 12:22:21 -0700

    simple router chooses whether to answer student's question or to generate a question

---
+++ app/router.py
@@ -0,0 +1,41 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import SystemMessage
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        SystemMessage(
+            content=(
+                "Given the conversation below, choose the next worker to continue the conversation."
+                " Choose from the following workers:\n"
+                "explain_word_or_phrase - Explain the meaning of a word or phrase if the student asks.\n"
+                "generate_question - Generate a question to test the students understanding when the indicate they are ready.\n"
+                " Do not respond with more than one word."
+            )
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+        # ("user", "{user_input}")
+    ]
+)
+
+chain = (
+    prompt
+    | ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    | StrOutputParser()
+)
+
+# print(chain.invoke({
+#     "messages": [
+#         "Praesente (bis) et Claudiano consulibus, sexto decimo kalendas Augustas, Carthagine in secretario impositis, Sperato, Nartzalo ... Saturninus proconsul dixit: 'Potestis indulgentiam domini nostri Imperatoris promereri, si ad bonam mentem redeatis.' nterroga me de significatione verborum vel locutionum, aut auxilium in intellegendo locum quaere. Cum paratus eris, responde 'paratus sum'.", 
+#         "Quid significat 'consulibus'?"
+#     ]
+# }))
+
+# print(chain.invoke({
+#     "messages": [
+#         "Praesente (bis) et Claudiano consulibus, sexto decimo kalendas Augustas, Carthagine in secretario impositis, Sperato, Nartzalo ... Saturninus proconsul dixit: 'Potestis indulgentiam domini nostri Imperatoris promereri, si ad bonam mentem redeatis.' nterroga me de significatione verborum vel locutionum, aut auxilium in intellegendo locum quaere. Cum paratus eris, responde 'paratus sum'.", 
+#         "paratus sum"
+#     ]
+# }))
